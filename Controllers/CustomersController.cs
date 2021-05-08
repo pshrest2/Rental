de72d01054bbcf83806d177ff534504bcf57785e
Pranaya Shrestha <55268350+pshrest2@users.noreply.github.com>
2021-05-08 11:41:52 -0500

    Add Customer To Database with Form

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -33,6 +33,15 @@ namespace MRent.Controllers
             return View(viewModel);
         }
 
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
