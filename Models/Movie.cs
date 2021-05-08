4aa18e7e8a29fbcd9bc33d7c296f05dc36b6c8df
Pranaya Shrestha <55268350+pshrest2@users.noreply.github.com>
2021-05-08 11:02:35 -0500

    Add new movie properties and display it

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -9,5 +9,9 @@ namespace MRent.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int Stock { get; set; }
     }
 }
