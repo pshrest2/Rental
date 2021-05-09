53858121ce70c0198779f4d086bd493526fc50a0
Pranaya Shrestha <55268350+pshrest2@users.noreply.github.com>
2021-05-09 17:54:55 -0500

    Code Maintainability. Tidy

--- Models/MembershipType.cs
+++ Models/MembershipType.cs
@@ -8,13 +8,16 @@ namespace MRent.Models
     public class MembershipType
     {
         public string Name { get; set; }
+
         public byte Id { get; set; }
+
         public short SignUpFee { get; set; }
 
         public byte DurationInMonths { get; set; }
 
         public byte DiscountRate { get; set; }
 
-
+        public static readonly byte Unknown = 0;
+        public static readonly byte PayAsYouGo = 1;
     }
 }
