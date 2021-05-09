07c673e648cc309a32c91bcb53c0090e49f687b5
Pranaya Shrestha <55268350+pshrest2@users.noreply.github.com>
2021-05-09 17:50:49 -0500

    Custom Validation

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -22,6 +22,7 @@ namespace MRent.Models
         public byte MembershipTypeId { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? BirthDate { get; set; }
     }
 }
