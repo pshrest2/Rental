2a445030204ebf6361abea1f69b4508929bd86a1
Pranaya Shrestha <55268350+pshrest2@users.noreply.github.com>
2021-05-09 17:38:55 -0500

    Add Validation

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -69,6 +69,17 @@ namespace MRent.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
+
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
+
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
             else
