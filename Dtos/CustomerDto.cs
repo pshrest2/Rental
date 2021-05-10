00498ccef56570b35ead41a020e88bc96c4d5d36
Pranaya Shrestha <55268350+pshrest2@users.noreply.github.com>
2021-05-09 21:06:30 -0500

    Add Data Transfer Objects

---
+++ Dtos/CustomerDto.cs
@@ -0,0 +1,25 @@
+﻿using MRent.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MRent.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Customer's Name")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewletter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? BirthDate { get; set; }
+    }
+}
