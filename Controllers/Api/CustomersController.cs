552043ee057069c9d82be6dd5c59a15b8f65ec7f
Pranaya Shrestha <55268350+pshrest2@users.noreply.github.com>
2021-05-10 10:39:08 -0500

    add membershipTypeDto to return hierarchial data

--- Controllers/Api/CustomersController.cs
+++ Controllers/Api/CustomersController.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Data.Entity;
 
 namespace MRent.Controllers.Api
 {
@@ -22,7 +23,11 @@ namespace MRent.Controllers.Api
         // GET /api/customers
         public IHttpActionResult GetCustomers()
         {
-            var customerDtos =  _context.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>);
+            var customerDtos =  _context.Customers
+                .Include(c => c.MembershipType)
+                .ToList()
+                .Select(Mapper.Map<Customer, CustomerDto>);
+
             return Ok(customerDtos);
         }
 
