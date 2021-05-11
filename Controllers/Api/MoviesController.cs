ed279856357dc945443c409053a83c78c7f26a10
Pranaya Shrestha <55268350+pshrest2@users.noreply.github.com>
2021-05-11 14:50:32 -0500

    Add Movies

--- Controllers/Api/MoviesController.cs
+++ Controllers/Api/MoviesController.cs
@@ -23,10 +23,16 @@ namespace MRent.Controllers.Api
 
 
         //GET /api/movies
-        public IHttpActionResult GetMovies()
+        public IHttpActionResult GetMovies(string query = null)
         {
-            var movieDtos =  _context.Movies
+            var moviesQuery = _context.Movies
                 .Include(m => m.Genre)
+                .Where(m => m.NumberAvailable > 0);
+
+            if (!String.IsNullOrWhiteSpace(query))
+                moviesQuery = moviesQuery.Where(m => m.Name.Contains(query));
+
+            var movieDtos =  moviesQuery
                 .ToList()
                 .Select(Mapper.Map<Movie, MovieDto>);
 
